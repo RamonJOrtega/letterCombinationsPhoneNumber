c9619f233f8b9fc1b66d2851f43f4d02c1eff4ee
ramon6767 <ramon.justis.ortega@gmail.com>
2023-03-28 15:20:21 -0700

    adds two-nine String arrays of phone digits

---
+++ src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> combos = new ArrayList<>();
+        String[] two = {"a", "b", "c"};
+        String[] three = {"d", "e", "f"};
+        String[] four = {"g", "h", "i"};
+        String[] five = {"j", "k", "l"};
+        String[] six = {"m", "n", "o"};
+        String[] seven = {"p", "q", "r", "s"};
+        String[] eight = {"t", "u", "v"};
+        String[] nine = {"w", "x", "y", "z"};
+
+        return combos;
+        
+    }
+}
